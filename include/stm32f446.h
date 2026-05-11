facafd4f424f9d951efba40d0a6786604fe81d92
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-12 01:48:33 +0200

    add peripheral base address definitions and register mapping

--- include/stm32f446.h
+++ include/stm32f446.h
@@ -3,13 +3,67 @@
 
 #include <stdint.h>
 
+/* MEMORY */
+#define FLASH_BASE            0x08000000UL
+#define SRAM_BASE             0x20000000UL
+#define PERIPH_BASE           0x40000000UL
 
-#define FLASH_BASE      0x08000000UL
-#define SRAM_BASE       0x20000000UL
+/* BUSS */
+#define APB1PERIPH_BASE       PERIPH_BASE
+#define APB2PERIPH_BASE       (PERIPH_BASE + 0x00010000UL)
+#define AHB1PERIPH_BASE       (PERIPH_BASE + 0x00020000UL)
+#define AHB2PERIPH_BASE       0x50000000UL
+#define AHB3PERIPH_BASE       0xA0000000UL
 
-/* Peripherals*/
-#define PERIPH_BASE     0x40000000UL
-#define AHB1PERIPH_BASE (PERIPH_BASE + 0x00020000UL)
+/* AHB1 Peripherals (GPIO, RCC, DMA) */
+#define GPIOA_BASE            (AHB1PERIPH_BASE + 0x0000UL)
+#define GPIOB_BASE            (AHB1PERIPH_BASE + 0x0400UL)
+#define GPIOC_BASE            (AHB1PERIPH_BASE + 0x0800UL)
+#define GPIOD_BASE            (AHB1PERIPH_BASE + 0x0C00UL)
+#define GPIOE_BASE            (AHB1PERIPH_BASE + 0x1000UL)
+#define GPIOF_BASE            (AHB1PERIPH_BASE + 0x1400UL)
+#define GPIOG_BASE            (AHB1PERIPH_BASE + 0x1800UL)
+#define GPIOH_BASE            (AHB1PERIPH_BASE + 0x1C00UL)
+#define CRC_BASE              (AHB1PERIPH_BASE + 0x3000UL)
+#define RCC_BASE              (AHB1PERIPH_BASE + 0x3800UL)
+#define FLASH_R_BASE          (AHB1PERIPH_BASE + 0x3C00UL)
+#define DMA1_BASE             (AHB1PERIPH_BASE + 0x6000UL)
+#define DMA2_BASE             (AHB1PERIPH_BASE + 0x6400UL)
 
+/* APB1 Peripherals (Timers, UARTs, I2C, SPI)  */
+#define TIM2_BASE             (APB1PERIPH_BASE + 0x0000UL)
+#define TIM3_BASE             (APB1PERIPH_BASE + 0x0400UL)
+#define TIM4_BASE             (APB1PERIPH_BASE + 0x0800UL)
+#define TIM5_BASE             (APB1PERIPH_BASE + 0x0C00UL)
+#define RTC_BASE              (APB1PERIPH_BASE + 0x2800UL)
+#define WWDG_BASE             (APB1PERIPH_BASE + 0x2C00UL)
+#define IWDG_BASE             (APB1PERIPH_BASE + 0x3000UL)
+#define SPI2_BASE             (APB1PERIPH_BASE + 0x3800UL)
+#define SPI3_BASE             (APB1PERIPH_BASE + 0x3C00UL)
+#define USART2_BASE           (APB1PERIPH_BASE + 0x4400UL)
+#define USART3_BASE           (APB1PERIPH_BASE + 0x4800UL)
+#define UART4_BASE            (APB1PERIPH_BASE + 0x4C00UL)
+#define UART5_BASE            (APB1PERIPH_BASE + 0x5000UL)
+#define I2C1_BASE             (APB1PERIPH_BASE + 0x5400UL)
+#define I2C2_BASE             (APB1PERIPH_BASE + 0x5800UL)
+#define I2C3_BASE             (APB1PERIPH_BASE + 0x5C00UL)
+#define PWR_BASE              (APB1PERIPH_BASE + 0x7000UL)
+#define DAC_BASE              (APB1PERIPH_BASE + 0x7400UL)
 
-#endif
+/* APB2 Peripherals (ADC, SYSCFG, SPI1, USART1/6) */
+#define TIM1_BASE             (APB2PERIPH_BASE + 0x0000UL)
+#define TIM8_BASE             (APB2PERIPH_BASE + 0x0400UL)
+#define USART1_BASE           (APB2PERIPH_BASE + 0x1000UL)
+#define USART6_BASE           (APB2PERIPH_BASE + 0x1400UL)
+#define ADC_BASE              (APB2PERIPH_BASE + 0x2000UL) // ADC1, 2, 3 common base
+#define SPI1_BASE             (APB2PERIPH_BASE + 0x3000UL)
+#define SYSCFG_BASE           (APB2PERIPH_BASE + 0x3800UL)
+#define EXTI_BASE             (APB2PERIPH_BASE + 0x3C00UL)
+
+/* (Pekare till structs) */
+#define GPIOA       ((GPIO_TypeDef *) GPIOA_BASE)
+#define GPIOB       ((GPIO_TypeDef *) GPIOB_BASE)
+#define GPIOC       ((GPIO_TypeDef *) GPIOC_BASE)
+#define RCC         ((RCC_TypeDef *) RCC_BASE)
+
+#endif /* STM32F446XX_H */
