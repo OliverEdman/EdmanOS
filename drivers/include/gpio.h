2535da3092847e415cae0ecccd254fa86cacd445
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-30 22:52:46 +0200

    test commit

---
+++ drivers/include/gpio.h
@@ -0,0 +1,9 @@
+/**
+ * @file gpio.h
+ * @brief interface for the gpio driver.
+ * @author Oliver Edman <o.edman@icloud.com>
+ * @date 2026
+ *
+ * @note Licensed under the MIT License (see LICENSE file in root).
+ * Copyright (c) 2026 Oliver Edman
+ */
