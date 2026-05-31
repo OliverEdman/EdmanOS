7e917120c4a6cb240493ee004a615b36848fdde2
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-31 02:34:21 +0200

    implemented interface for gpio driver and added Doxygen comments

--- include/gpio.h
+++ include/gpio.h
@@ -7,3 +7,60 @@
  * @note Licensed under the MIT License (see LICENSE file in root).
  * Copyright (c) 2026 Oliver Edman
  */
+
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/**
+ * enum gpio_direction - Defines the direction for a GPIO pin.
+ * @GPIO_DIRECTION_INPUT: Input mode without internal pull-up.
+ * @GPIO_DIRECTION_INPUT_PULLUP: Input mode with internal pull-up enabled.
+ * @GPIO_DIRECTION_OUTPUT: Output mode.
+ */
+enum gpio_direction {
+	GPIO_DIRECTION_INPUT,
+	GPIO_DIRECTION_INPUT_PULLUP,
+	GPIO_DIRECTION_OUTPUT,
+};
+
+struct gpio;
+
+/**
+ * gpio_new() - Creates a new GPIO instance.
+ * @pin: Pin number on the MCU.
+ * @direction: Desired direction (see &enum gpio_direction).
+ *
+ * Return: A pointer to a newly allocated &struct gpio instance.
+ */
+struct gpio gpio_new(uint8_t pin, enum gpio_direction direction);
+
+/**
+ * gpio_delete() - Frees a GPIO instance and nullifies the pointer.
+ * @self: Double pointer to the GPIO object to be freed.
+ *
+ * Sets the pointer to NULL after deallocation to prevent dangling pointers.
+ */
+void gpio_delete(struct gpio **self);
+
+/**
+ * gpio_write() - Sets the state of a GPIO pin.
+ * @self: Pointer to the GPIO object.
+ * @state: Boolean value (true for high, false for low).
+ */
+void gpio_write(struct gpio *self, bool state);
+
+/**
+ * gpio_read() - Reads the state of a GPIO pin.
+ * @self: Pointer to the GPIO object.
+ *
+ * Return: true if the pin is high, false if the pin is low.
+ */
+bool gpio_read(const struct gpio *self);
+
+/**
+ * gpio_toggle() - Toggles the state of a GPIO pin.
+ * @self: Pointer to the GPIO object.
+ */
+void gpio_toggle(struct gpio *self);
