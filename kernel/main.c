558a843db5142b1e8fa904611d54cc1585b6c4fe
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-03 23:58:06 +0200

    gpio driver test simple main code

--- kernel/main.c
+++ kernel/main.c
@@ -9,25 +9,17 @@ void fast_delay(uint32_t count) {
 }
 
 int main(void) {
-    // starta klocka vväck hårdvara
+    
     volatile uint32_t *RCC_AHB1ENR = (uint32_t *)0x40023830;
     *RCC_AHB1ENR |= (1U << 0); 
 
-    /* Skapa GPIO objekt (D2) */
     struct gpio *led = gpio_new(GPIOA, 10U, GPIO_MODE_OUTPUT);
 
-    if (led != NULL) {
-        gpio_write(led, true);  
-        fast_delay(500000U);
-        
-        gpio_write(led, false);
-        fast_delay(500000U);
-    }
-
+    
     while(1) {
         if (led != NULL) {
-            gpio_toggle(led);
+            gpio_toggle(led); 
+            fast_delay(1000000U); 
         }
-        fast_delay(200000U);
     }
 }
